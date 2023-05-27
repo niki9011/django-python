0f7faa79327b5e3d2f3f98f2c3b5522c6eb38fed
niki9011 <niki901115@gmail.com>
2023-05-27 22:00:07 +0300

    urls update

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from petstagram.accounts.views import register_user, login_user, profile_details, profile_edit, profile_delete
+from .views import register_user, login_user, profile_details, profile_edit, profile_delete
 
 urlpatterns = [
-    path('register/', register_user, name='register user'),
-    path('login/', login_user, name='login user'),
+
+    path('register/', register_user, name="register user"),
+    path('login/', login_user, name="login user"),
     path('profile/<int:pk>/', include([
         path('', profile_details, name='profile details'),
         path('edit/', profile_edit, name='profile edit'),
-        path('delete/', profile_delete, name='profile delete'),
+        path('delete/', profile_delete, name='profile delete')
     ]))
 ]
